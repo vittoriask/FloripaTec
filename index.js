fe526828a3d09896e7baf9e7eec3f5bceda269f7
vittoriask <viter9292@gmail.com>
2022-10-22 23:55:02 -0300

    exercises

---
+++ index.js
@@ -0,0 +1,13 @@
+// let name = "Vitória";
+// let age = 20;
+// let profission = "Dev Junior";
+// let obj = "Aprimorar os conhecimentos em desenvolvimento e engressar no mercado de trabalho.";
+
+
+// let idade = Number(prompt("Informe sua idade"))
+
+// if (idade >= 18){
+//     alert('Maior de idade!')
+// }else if(idade < 18){
+//     alert('Menor de idade!')
+// }
