f7903265a243f727930d26f7c032585ad49bca0e
vittoriask <viter9292@gmail.com>
2022-10-24 00:17:09 -0300

    att exercises

--- index.js
+++ index.js
@@ -11,3 +11,50 @@
 // }else if(idade < 18){
 //     alert('Menor de idade!')
 // }
+
+function calculaMedia(){
+    if(document.getElementById('nota1').value= '' || document.getElementById('nota2').value == ''
+    || document.getElementById('nota3').value == '' || document.getElementById('nota4').value == ''){
+
+        alert('Preencha todas as notas!')
+    } else{
+        let nota1 = Number(document.getElementById('nota1').value)
+        let nota2 = Number(document.getElementById('nota2').value)
+        let nota3 = Number(document.getElementById('nota3').value)
+        let nota4 = Number(document.getElementById('nota4').value)
+
+        let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+        if(media < 6){
+            alert('Média: ' + media + ', aluno reprovado')
+            document.getElementById('resultado').textContent = 'Média: ' + media + ', aluno reprovado.'
+            limparCampos()
+        }else if(media >= 6 && media < 7){
+            alert('Média: ' + media + ', aluno em recuperação!')
+            document.getElementById('resultado').textContent = 'Média: ' + media + ', aluno em recuperação!'
+            limparCampos()
+        }else{
+            alert('Média: ' + media + ', aluno aprovado.')
+            document.getElementById('resultado').textContent = 'Média: ' + media + ', aluno aprovado.'
+            limparCampos()
+        }
+    }
+
+
+    function limparCampos(){
+        document.getElementById('nota1').value='';
+        document.getElementById('nota2').value='';
+        document.getElementById('nota3').value='';
+        document.getElementById('nota4').value='';
+    }
+
+
+
+
+
+
+}
+
+// Apresenta ERRO!!!! ARRUMAR!!!
+
+
